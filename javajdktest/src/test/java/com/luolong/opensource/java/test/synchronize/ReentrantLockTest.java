82697cffcf91d2683fb803e5acb0069e315dd3ea
luolong <luolong@jinhuhang.com.cn>
2018-12-26 11:42:54 +0800

    可重入锁test类提交

--- javajdktest/src/test/java/com/luolong/opensource/java/test/synchronize/ReentrantLockTest.java
+++ javajdktest/src/test/java/com/luolong/opensource/java/test/synchronize/ReentrantLockTest.java
@@ -1,8 +1,10 @@
 package com.luolong.opensource.java.test.synchronize;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -10,7 +12,9 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * <p>可重入锁测试类</p>
- * 1,公平锁，非公平锁
+ * 1，可重入锁：可重入的意思就是一个线程可以多次进入自己的方法而不阻塞
+ * 2,公平锁，非公平锁
+ *
  * @author luolong
  * @date 2018/12/25
  */
@@ -20,12 +24,52 @@ public class ReentrantLockTest {
     private static BoundedBuffer boundedBuffer = new BoundedBuffer();
     private static ReentrantLock reentrantLock1 = new ReentrantLock();
 
+    /**
+     * 可重入锁公平性非公平性测试
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             consumerTest();
             chiliFactoryTest();
         }
 
+    }
+
+    /**
+     * 可重入锁，就是同一个线程可以多次进入,而且可以拿到进入的次数
+     * 进入多少次就必须unlock多少次
+     * @param
+     * @return void
+     * @author long.luo
+     * @date 2018/12/26
+     */
+    @Test
+    public void reentrantLockTest() throws InterruptedException {
+        class BoundedBuffer1 {
+            final ReentrantLock lock1 = new ReentrantLock(true);
+            final Condition notFull = lock1.newCondition();
+            final Condition notEmpty = lock1.newCondition();
+            final Object[] items = new Object[3];
+            int putptr, takeptr, count;
+
+            public void put(Object x) throws InterruptedException {
+                lock1.lock();
+                lock1.lock();
+                try {
+                    System.out.println(Thread.currentThread().getName() + ": " + lock1.getHoldCount());
+                    TimeUnit.SECONDS.sleep(10);
+                } finally {
+                    lock1.unlock();
+                    lock1.unlock();
+                }
+            }
+        }
+        BoundedBuffer1 boundedBuffer = new BoundedBuffer1();
+        for (int i = 0; i < 3; i++) {
+            boundedBuffer.put(i);
+        }
     }
 
     public static void consumerTest() {
@@ -64,6 +108,7 @@ public class ReentrantLockTest {
 
         final Object[] items = new Object[3];
         int putptr, takeptr, count;
+
 
         public void put(Object x) throws InterruptedException {
             reentrantLock1.lock();
@@ -105,10 +150,6 @@ public class ReentrantLockTest {
             } finally {
                 lock.unlock();
             }
-        }
-
-        public void printlnLockThread() {
-
         }
     }
 }
