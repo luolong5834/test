bab7eba26e65bfb72dc1fd2aee94f0a0cb1fcab4
luolong <ll@zihebest.com>
2019-08-14 19:35:33 +0800

    hey boy 2

--- javajdktest/src/test/java/com/luolong/opensource/java/test/DB/JDBCTest.java
+++ javajdktest/src/test/java/com/luolong/opensource/java/test/DB/JDBCTest.java
@@ -10,8 +10,7 @@ import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-
+import java.sql.SQLException;12
 */
 /**
  * <p></p>
